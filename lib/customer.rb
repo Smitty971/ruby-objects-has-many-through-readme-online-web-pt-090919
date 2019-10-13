a8e64729f88376e541e3f4705ff7921befda929c
Jamal Turner <jlsmitty2273@yahoo.com>
2019-10-13 17:28:17 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -12,8 +12,8 @@ class Customer
     @@all
   end
 
-    def new_meal(self, waiter, total, tip = 0)
-      Meal.new(waiter, total, tip)
+    def new_meal(waiter, total, tip = 0)
+      Meal.new(self, waiter, total, tip)
     end
 
 
