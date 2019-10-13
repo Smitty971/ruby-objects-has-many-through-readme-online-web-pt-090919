fcd0ae99a927b1d1bc9cf715c6375faae7a0f6c9
Jamal Turner <jlsmitty2273@yahoo.com>
2019-10-13 17:50:02 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -22,4 +22,11 @@ attr_accessor :name, :years_of_experience
     end
   end
 
+  def best_tipper
+    best_tipped_meal = meals.max do | meal_a, meal_b|
+      meal_a.tip <=> meal_b.tip
+    end
+
+    best_tipped_meal.customer
+
 end
