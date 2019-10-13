fd0576e3d9900c815998dfdb54fad699606afc7d
Jamal Turner <jlsmitty2273@yahoo.com>
2019-10-13 17:03:17 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,13 @@
 class Waiter
 
+@@all = []
+
+  def initialize(name, years_of_experience)
+    @name = name
+    @years_of_experience = years_of_experience
+  end
+
+  def self.all
+    @@all
+  end 
 end
