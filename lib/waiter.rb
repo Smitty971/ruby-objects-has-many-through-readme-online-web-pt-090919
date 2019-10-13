155c95df3461a6ab2333c39e43fd8c5ff32584e5
Jamal Turner <jlsmitty2273@yahoo.com>
2019-10-13 17:34:56 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,14 +1,18 @@
 class Waiter
-
+attr_accessor :name, :years_of_experience
 @@all = []
 
   def initialize(name, years_of_experience)
     @name = name
     @years_of_experience = years_of_experience
-    @@all << self 
+    @@all << self
   end
 
   def self.all
     @@all
+  end
+
+  def new_meal(customer, self, total, tip = 0)
+    Meal.New(customer, self, total, tip = 0)
   end
 end
